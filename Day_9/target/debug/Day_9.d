198773b9e91a229a1bb7a624029f00c381612692
Zml99 <malinares99@gmail.com>
2020-12-09 00:02:39 -0400

    Handling errors with panic! and a little of Result<T,E>

---
+++ Day_9/target/debug/Day_9.d
@@ -0,0 +1,1 @@
+E:\100DayChallenge\Day_9\target\debug\Day_9.exe: E:\100DayChallenge\Day_9\src\main.rs
---
+++ Day_9/target/debug/deps/Day_9.d
@@ -0,0 +1,5 @@
+E:\100DayChallenge\Day_9\target\debug\deps\Day_9.exe: src\main.rs
+
+E:\100DayChallenge\Day_9\target\debug\deps\Day_9.d: src\main.rs
+
+src\main.rs:
